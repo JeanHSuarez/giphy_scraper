19679e83ae8570f52811446ea182b9ec12ba3010
JeanHSuarez <devyoyong@gmailcom>
2021-01-01 19:03:58 -0500

    Refactoring Functions Using Pipes!

---
+++ config/config.exs
@@ -0,0 +1,3 @@
+use Mix.Config
+
+config :giphy_scraper, :api_key, "tbH6BNzvYf6c2chgNl3kLIAmPa4r8iFZ"
--- lib/data_processing/data_processing.ex
+++ lib/data_processing/data_processing.ex
@@ -3,19 +3,18 @@ defmodule GiphyImage do
 end
 
 defmodule DataProcessing.DataProcessing do
-	def request_data(anything) do
-		response = HTTPoison.get!"https://api.giphy.com/v1/gifs/search?api_key=tbH6BNzvYf6c2chgNl3kLIAmPa4r8iFZ&q=#{anything}&limit=25&offset=0&rating=g&lang=en"
-		HTTPoison.Handlers.Multipart.decode_body(response)
+		
+	def search(anything) do
+		api_key = Application.get_env(:giphy_scraper, :api_key)
+		%{"data" => value} = "https://api.giphy.com/v1/gifs/search?api_key=#{api_key}&q=#{anything}&limit=25&offset=0&rating=g&lang=en" 
+		|> HTTPoison.get!
+		|> HTTPoison.Handlers.Multipart.decode_body
+		|> Jason.decode!
+		Enum.map(value, fn v -> populate_struct(v) end)
 	end
 
 	def populate_struct(%{"id" => id, "url" => url, "username" => username, "title" => title}) do
 		%GiphyImage{id: to_string(id), url: to_string(url), username: to_string(username), title: to_string(title)}
 	end
-
-	def search(query) do
-		fetched_data = request_data(query)
-		result = Jason.decode!(fetched_data)
-		%{"data" => value} = result
-		Enum.map(value, fn v -> populate_struct(v) end)
-	end
+	
 end
